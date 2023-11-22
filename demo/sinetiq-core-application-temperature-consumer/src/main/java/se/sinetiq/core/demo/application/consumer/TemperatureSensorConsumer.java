3527cfa8dd5b34347e33ed3d8a93cd4e06744b6e
Fredrik Blomstedt <fredrik.blomstedt@sinetiq.se>
2023-11-22 13:19:53 +0100

    Consumer uses Lookup (discovery)

--- demo/sinetiq-core-application-temperature-consumer/src/main/java/se/sinetiq/core/demo/application/consumer/TemperatureSensorConsumer.java
+++ demo/sinetiq-core-application-temperature-consumer/src/main/java/se/sinetiq/core/demo/application/consumer/TemperatureSensorConsumer.java
@@ -1,16 +1,57 @@
 package se.sinetiq.core.demo.application.consumer;
 
+import java.util.List;
+
 import se.sinetiq.core.demo.api.client.ApiClient;
 import se.sinetiq.core.demo.api.client.ApiException;
 import se.sinetiq.core.demo.api.client.Configuration;
 import se.sinetiq.core.demo.api.client.model.TemperatureData;
+import se.sinetiq.core.sr.consul.api.ConsulAPI;
 import se.sinetiq.core.demo.api.client.api.DefaultApi;
+
+import se.sinetiq.core.sr.consul.api.ServiceType;
+import se.sinetiq.core.sr.consul.api.ServiceName;
+import se.sinetiq.core.sr.consul.api.ServiceData;
 
 public class TemperatureSensorConsumer {
 
   public static void main(String[] args) {
+
+    ConsulAPI consulAPI = new ConsulAPI();
+    //(environment.getProperty("sr.host"), environment.getProperty("sr.port")
+    if(consulAPI.configure("localhost","8500") == false) {
+      System.out.println("Service registry configuration failure!");
+      return;
+    }
+
+    //
+    // Part 1: Discover (lookup) services (api instances) that fulfill the api specification 'temperature-sensor'
+    //
+    ServiceType serviceType = new ServiceType("temperature-sensor-rest-json");
+    List<ServiceName> apiInstances = consulAPI.getServiceInstances(serviceType);
+    System.out.println("Found " + apiInstances.size() + " instaces.");
+    for(ServiceName sn : apiInstances) {
+      // Found service
+      System.out.println("+ " + sn.getName());
+    }
+
+    if(apiInstances.size() <= 0) {
+      System.out.println("No services found, abort");
+      return;
+    }
+
+    //
+    // Part 2: Use discovery endpoint details for establish connection
+    //
+    ServiceName sn = apiInstances.get(0); // Just pick the first service (api instance) found
+    ServiceData sd = consulAPI.getServiceData(sn);
+    if(sd == null) {
+      System.out.println("No services data found, abort");
+      return;
+    }
     ApiClient defaultClient = Configuration.getDefaultApiClient();
-    defaultClient.setBasePath("http://127.0.0.1:8080");
+    String pathStr = sd.getProperties().get("path")!=null?sd.getProperties().get("path"):"";
+    defaultClient.setBasePath("http://" + sd.getHost() + ":" + sd.getPort() /*+ pathStr*/);
 
     DefaultApi apiInstance = new DefaultApi(defaultClient);
     try {
